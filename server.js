29e2e6257374caedd8737d9a88c09796378c44ee
Argeoff <214428272+Argeoff@users.noreply.github.com>
2026-04-17 20:41:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
+app.options("*", cors()); // handles preflight
 app.use(express.json());
 
 // current room state
